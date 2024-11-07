a3465b7d16f00c65aa7e2609b108fcfeb2717f68
Rachel <160640962+rachel-caille@users.noreply.github.com>
2024-11-07 09:51:03 +0000

    Exercice 5 : Palindromes

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+"""On veut avoir un palindrome"""
+import string
+from unidecode import unidecode
+
 #### Fonction secondaire
 
-
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+def ispalindrome(s):
+    """L'objectif est de vérifier si s est un palindrome"""
+    s = unidecode(s.replace(" ", "").lower())
+    s = s.translate(str.maketrans("", "", string.punctuation))
+    return s == s[::-1]
 
 #### Fonction principale
 
-
 def main():
-
     # vos appels à la fonction secondaire ici
-
+    """On teste sur des chaînes de caractères la fonction ispalindrome()"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
